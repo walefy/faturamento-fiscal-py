9e8ed8a176780e2038eb6113e8301606a128a84a
walefy <walefyd1@gmail.com>
2023-11-26 10:41:02 -0300

    style: remove some comments

--- app/schemas/root_get_schema.py
+++ app/schemas/root_get_schema.py
@@ -1,6 +1,3 @@
-# from pydantic import BaseModel, Field, field_validator, validator
-# from datetime import date, timedelta
-
 from datetime import date
 
 from pydantic import BaseModel, Field
@@ -20,28 +17,3 @@ class PdfRegister(BaseModel):
     start_time: date
     end_time: date
     values: list[str]
-
-    # @field_validator('start_time')
-    # @classmethod
-    # def start_time_is_valid(cls, value):
-    #     if value > cls.end_time:
-    #         raise ValueError('start_time must be less than end_time')
-    #     return value
-
-    # @field_validator('end_time')
-    # @classmethod
-    # def end_time_is_valid(cls, value):
-    #     if value < cls.start_time:
-    #         raise ValueError('end_time must be greater than start_time')
-    #     return value
-
-    # @validator('start_time', 'end_time')
-    # @classmethod
-    # def date_range_is_valid(cls, value):
-    #     if cls.start_time and cls.end_time:
-    #         twenty_four_months_in_days = 730
-    #         delta = cls.end_time - cls.start_time
-    #         if delta > timedelta(days=twenty_four_months_in_days):
-    #             raise ValueError(
-    #              'The date range must be less than 24 months')
-    #     return value
